5cbc6211ea5c029a14e29483a0989ae85cd2ab3f
Nitesh <niteshshetye204@gmail.com>
2021-07-22 11:50:13 +0530

    Base Code

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+  return <div>Hellow Frindship With Coding</div>;
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+// Component
+import App from "./App";
+
+ReactDom.render(<App />, document.getElementById("root"));
